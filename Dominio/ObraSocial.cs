7b4f1632cb27d646f2e2c2b05ae101aae2d51971
Tomás Carrasquero <cardenalthompson@hotmail.com>
2021-11-28 21:10:14 -0300

    cosas

---
+++ Dominio/ObraSocial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ObraSocial
+    {
+		public string Rnos { get; set; }
+		public string Nombre { get; set; }
+		public string Sigla { get; set; }
+		public string Domicilio { get; set; }
+		public int CodigoPostal { get; set; }
+		public string Email { get; set; }
+
+
+    }
+}
